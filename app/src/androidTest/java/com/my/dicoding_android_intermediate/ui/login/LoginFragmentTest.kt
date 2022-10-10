d33a771d1e21814de156880f649dd9ded4d87015
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2022-10-10 17:26:39 +0700

    perbaikan

--- app/src/androidTest/java/com/my/dicoding_android_intermediate/ui/login/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/my/dicoding_android_intermediate/ui/login/LoginFragmentTest.kt
@@ -1,14 +1,16 @@
 package com.my.dicoding_android_intermediate.ui.login
 
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.my.dicoding_android_intermediate.ConfigRunnerTest
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 @HiltAndroidTest
 class LoginFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Test
     fun getHeadlineNews_Success() {
