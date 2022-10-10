459a9e320130dd6b8b7c92301f4635943fe55383
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2022-10-10 14:45:02 +0700

    perbaikan

---
+++ app/src/androidTest/java/com/my/dicoding_android_intermediate/ui/login/LoginFragmentTest.kt
@@ -0,0 +1,9 @@
+package com.my.dicoding_android_intermediate.ui.login
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginFragmentTest {
+
+}
