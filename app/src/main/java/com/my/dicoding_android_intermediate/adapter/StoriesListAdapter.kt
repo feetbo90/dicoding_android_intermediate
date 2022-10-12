5b833b9f01435aa8cb89fd80f125fa2a8514b3e3
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2022-10-12 20:16:59 +0700

    perbaikan

---
+++ app/src/main/java/com/my/dicoding_android_intermediate/adapter/StoriesListAdapter.kt
@@ -0,0 +1,7 @@
+package com.my.dicoding_android_intermediate.adapter
+
+import androidx.paging.PagingDataAdapter
+import com.my.dicoding_android_intermediate.data.entities.Story
+
+class StoriesListAdapter: PagingDataAdapter<Story, StoryI> {
+}
