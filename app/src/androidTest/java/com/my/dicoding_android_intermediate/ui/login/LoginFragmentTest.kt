189cc605b42a4526ba91831fe5b39f1418fa9d07
Angga Hasibuan <dickyhasibuan@Dickys-MacBook-Pro.local>
2022-10-14 00:10:55 +0700

    t

--- app/src/androidTest/java/com/my/dicoding_android_intermediate/ui/login/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/my/dicoding_android_intermediate/ui/login/LoginFragmentTest.kt
@@ -1,14 +1,20 @@
 package com.my.dicoding_android_intermediate.ui.login
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.my.dicoding_android_intermediate.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.CustomTestApplication
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 
 @HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
 class LoginFragmentTest {
 
     @get:Rule
@@ -17,5 +23,7 @@ class LoginFragmentTest {
     @Test
     fun getHeadlineNews_Success() {
         launchFragmentInHiltContainer<LoginFragment>()
+
+        onView(withId(R.id.))
     }
 }
